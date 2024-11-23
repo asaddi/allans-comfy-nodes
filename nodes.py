4d6d3fb242f1804895dd817acaf151f9bca372b3
Allan Saddi <allan@saddi.com>
2024-11-22 16:42:29 -0800

    Add simple (& lazy) "Dump To Console" node.

--- nodes.py
+++ nodes.py
@@ -1,7 +1,51 @@
 from enum import Enum, auto
 import math
+from pprint import pprint
 
 from comfy_execution.graph import ExecutionBlocker
+
+
+class DumpToConsole:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "use_pprint": (
+                    "BOOLEAN",
+                    {
+                        "default": True,
+                        "label_on": "pprint",
+                        "label_off": "raw",
+                    },
+                ),
+                "input": ("*",),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+            },
+        }
+
+    @classmethod
+    def VALIDATE_INPUTS(cls, input_types):
+        return True
+
+    TITLE = "Dump To Console"
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    FUNCTION = "run"
+
+    CATEGORY = "private/debug"
+
+    def run(self, use_pprint, input, unique_id):
+        print(f"DumpToConsole (#{unique_id}) = ", end="")
+        if use_pprint:
+            pprint(input)
+        else:
+            print(input)
+
+        return ()
 
 
 # A clean-room low-rent copy of rgthree's seed node.
@@ -496,6 +540,7 @@ class ResolutionChooser:
 
 
 NODE_CLASS_MAPPINGS = {
+    "DumpToConsole": DumpToConsole,
     "PrivateSeed": PrivateSeed,
     "SimpleBus": SimpleBus,
     "ReproducibleWildcards": ReproducibleWildcards,

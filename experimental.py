f9a0a71db7422e98ed87c931b24f7e0ad8f159bb
Allan Saddi <allan@saddi.com>
2024-11-23 01:36:37 -0800

    DumpToConsole: Move to experimental.

--- experimental.py
+++ experimental.py
@@ -1,4 +1,49 @@
+from pprint import pprint
+
 import torch
+
+
+class DumpToConsole:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "use_pprint": (
+                    "BOOLEAN",
+                    {
+                        "default": True,
+                        "label_on": "pprint",
+                        "label_off": "raw",
+                    },
+                ),
+                "input": ("*",),
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+            },
+        }
+
+    @classmethod
+    def VALIDATE_INPUTS(cls, input_types):
+        return True
+
+    TITLE = "Dump To Console"
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    FUNCTION = "run"
+
+    CATEGORY = "private/debug"
+
+    def run(self, use_pprint, input, unique_id):
+        print(f"DumpToConsole (#{unique_id}) = ", end="")
+        if use_pprint:
+            pprint(input)
+        else:
+            print(input)
+
+        return ()
 
 
 class CLIPDistance:
@@ -33,5 +78,6 @@ class CLIPDistance:
 
 
 NODE_CLASS_MAPPINGS = {
+    "DumpToConsole": DumpToConsole,
     "CLIPDistance": CLIPDistance,
 }

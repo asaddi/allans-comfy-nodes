7edca1d5a52ffcfeccefe1309e3123f09327cd28
Allan Saddi <allan@saddi.com>
2024-11-24 18:45:59 -0800

    Add (fixed-input) Any Switches.

--- experimental.py
+++ experimental.py
@@ -2,6 +2,65 @@ from pprint import pprint
 
 import torch
 
+from comfy_execution.graph import ExecutionBlocker
+
+
+class AnyType(str):
+    def __ne__(self, other):
+        if self == "*":
+            # If we're the wildcard, match anything
+            return False
+        return super().__ne__(other)
+
+
+# Server-side-only implementation of an "any switch"
+class PrivateAnySwitch:
+    # Being server-side-only, it means we have a fixed number of inputs
+    NUM_INPUTS = 2
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        d = {
+            "optional": {
+            },
+            "hidden": {
+                "unique_id": "UNIQUE_ID",
+            },
+        }
+
+        for i in range(cls.NUM_INPUTS):
+            d["optional"][f"input{i + 1}"] = ("*",)
+
+        return d
+
+    @classmethod
+    def VALIDATE_INPUTS(cls, input_types):
+        # We've had luck using this to prevent validation errors on our
+        # wildcard inputs, so we'll just stick with it.
+        # (As opposed to using the str subclass, as done below.)
+        return True
+
+    TITLE = "Any Switch 2 (private)"
+
+    RETURN_TYPES = (AnyType("*"),)
+
+    FUNCTION = "switch"
+
+    CATEGORY = "private/switch"
+
+    def switch(self, unique_id, **kwargs):
+        for i in range(self.NUM_INPUTS):
+            input = kwargs.get(f"input{i + 1}")
+            if input is not None:
+                return (input,)
+        return (ExecutionBlocker(f"{self.TITLE} (#{unique_id}): All inputs muted"),)
+
+
+class PrivateAnySwitch4(PrivateAnySwitch):
+    NUM_INPUTS = 4
+
+    TITLE = "Any Switch 4 (private)"
+
 
 class DumpToConsole:
     @classmethod
@@ -78,6 +137,8 @@ class CLIPDistance:
 
 
 NODE_CLASS_MAPPINGS = {
+    "AnySwitch2": PrivateAnySwitch,
+    "AnySwitch4": PrivateAnySwitch4,
     "DumpToConsole": DumpToConsole,
     "CLIPDistance": CLIPDistance,
 }

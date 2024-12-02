d68c7eb5e035c10de15c3793f4046ac44a098408
Allan Saddi <allan@saddi.com>
2024-12-01 19:08:33 -0800

    dav2: Do full input/output list processing for efficiency.

--- dav2_nodes.py
+++ dav2_nodes.py
@@ -86,54 +86,64 @@ class DepthAnythingV2Node:
 
     TITLE = "DepthAnythingV2 Run"
 
+    INPUT_IS_LIST = True
+
     RETURN_TYPES = ("IMAGE",)
+    OUTPUT_IS_LIST = (True,)
 
     FUNCTION = "run"
 
     CATEGORY = "private/image"
 
     def run(self, dav2_model, image, grayscale):
+        dav2_model = dav2_model[0]
+        grayscale = grayscale[0]
+
         torch_device = get_torch_device()
         offload_device = unet_offload_device()
 
-        # model expects 8-bit BGR
-        image = (255.0 * image).clamp(0, 255).to(torch.uint8)
-        # Switch from RGB to BGR
-        image = image[:, :, :, [2, 1, 0]]
-
         dav2_model.to(torch_device)
 
-        depth_maps = []
-        for img in image:
-            # Note: It expects a numpy array.
-            # But it will convert it to a tensor and conditionally move
-            # it to cuda/mps/cpu. So the model better be in the same place!
-            depth = dav2_model.infer_image(img.numpy())
+        results: list[torch.Tensor] = []
+        for batched_image in image:
+            # model expects 8-bit BGR
+            batched_image = (255.0 * batched_image).clamp(0, 255).to(torch.uint8)
+            # Switch from RGB to BGR
+            batched_image = batched_image[:, :, :, [2, 1, 0]]
 
-            # Note: And we get back a numpy array
-            depth = torch.tensor(depth, dtype=torch.float32)
-            # Rescale to [0, 1]
-            depth = (depth - depth.min()) / (depth.max() - depth.min())
+            depth_maps = []
+            for img in batched_image:
+                # Note: It expects a numpy array.
+                # But it will convert it to a tensor and conditionally move
+                # it to cuda/mps/cpu. So the model better be in the same place!
+                depth = dav2_model.infer_image(img.numpy())
 
-            if grayscale:
-                # From [H,W] to [H,W,C] where C=1
-                depth = depth.unsqueeze(-1)
-                # Repeat channel for grayscale
-                depth = depth.expand(-1, -1, 3)
-            else:
-                cmap = mpl.colormaps["Spectral_r"]
-                depth = cmap(depth)[:, :, :3]  # Get rid of alpha
-                # And back to a tensor
+                # Note: And we get back a numpy array
                 depth = torch.tensor(depth, dtype=torch.float32)
+                # Rescale to [0, 1]
+                depth = (depth - depth.min()) / (depth.max() - depth.min())
 
-            # Finally, make batch of 1...
-            depth = depth.unsqueeze(0)
+                if grayscale:
+                    # From [H,W] to [H,W,C] where C=1
+                    depth = depth.unsqueeze(-1)
+                    # Repeat channel for grayscale
+                    depth = depth.expand(-1, -1, 3)
+                else:
+                    cmap = mpl.colormaps["Spectral_r"]
+                    depth = cmap(depth)[:, :, :3]  # Get rid of alpha
+                    # And back to a tensor
+                    depth = torch.tensor(depth, dtype=torch.float32)
 
-            depth_maps.append(depth)
+                # Finally, make batch of 1...
+                depth = depth.unsqueeze(0)
+
+                depth_maps.append(depth)
+
+            results.append(torch.cat(depth_maps))
 
         dav2_model.to(offload_device)
 
-        return (torch.cat(depth_maps),)
+        return (results,)
 
 
 NODE_CLASS_MAPPINGS = {

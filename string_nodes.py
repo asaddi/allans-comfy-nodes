1312d556d1ea7f80d7bf11d7ad80d12f72cfac72
Allan Saddi <allan@saddi.com>
2024-12-23 03:41:07 -0800

    Add very basic Save Tabular Data node.

--- string_nodes.py
+++ string_nodes.py
@@ -1,4 +1,71 @@
+import csv
+import os
 from pathlib import Path
+
+import folder_paths
+
+
+class SaveTabular:
+    def __init__(self):
+        self.output_dir = folder_paths.get_output_directory()
+
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "data": ("TABULAR",),
+                "filename_prefix": (
+                    "STRING",
+                    {
+                        "default": "ComfyUI",
+                    },
+                ),
+                "delimiter": (
+                    "BOOLEAN",
+                    {
+                        "label_on": "tab",
+                        "label_off": "comma",
+                        "default": True,
+                    },
+                ),
+            },
+        }
+
+    TITLE = "Save Tabular Data"
+
+    # TODO This is the easiest route, but it means each file will only contain
+    # data from a single prompt.
+    # Might need some way to accumulate rows into a single file?
+    INPUT_IS_LIST = True
+
+    RETURN_TYPES = ()
+    OUTPUT_NODE = True
+
+    FUNCTION = "save_tabular"
+
+    CATEGORY = "private/string"
+
+    def save_tabular(
+        self, data: list[list[str]], filename_prefix: list[str], delimiter: list[bool]
+    ):
+        filename_prefix: str = filename_prefix[0]
+        delimiter: bool = delimiter[0]
+
+        # We'll borrow the same method SaveImage uses
+        full_output_folder, filename, counter, subfolder, filename_prefix = (
+            folder_paths.get_save_image_path(filename_prefix, self.output_dir)
+        )
+
+        ext = "tsv" if delimiter else "csv"
+        file = f"{filename}_{counter:05}_.{ext}"
+        with open(os.path.join(full_output_folder, file), "w", newline="") as out:
+            # We'll try excel-tab for now, otherwise we might have to come
+            # up with our own dialect.
+            csvwriter = csv.writer(out, dialect="excel-tab" if delimiter else "excel")
+            for row in data:
+                csvwriter.writerow(row)
+
+        return ()
 
 
 class TabularJoin:
@@ -106,6 +173,7 @@ class PathJoin:
 
 
 NODE_CLASS_MAPPINGS = {
+    "SaveTabular": SaveTabular,
     "TabularJoin": TabularJoin,
     "PathSplit": PathSplit,
     "PathJoin": PathJoin,
